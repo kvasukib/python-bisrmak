f3dbbd7afae3b7e702236d675795d76d506fc695
Alexander Gamero-Garrido <agamerog@caida.org>
2016-03-24 00:01:15 -0700

    my code to parse bdrmap routers

---
+++ imc16plots/reading-routers.py
@@ -0,0 +1,16 @@
+import sys
+import re
+import os
+import getopt
+from sets import Set
+from  myexceptions import *
+from bdrmap_parse import * 
+
+routers = []
+interface2rtr = {}
+interface2name = {}
+
+filename = str(sys.argv[1])
+(routers,interface2rtr,interface2name) = read_bdrmap_file(filename)
+print(routers[0])
+
