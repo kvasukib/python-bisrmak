63f006397942be132ff900cd3bb633ca5c25531c
Alexander Gamero-Garrido <gamero@alum.mit.edu>
2016-08-19 18:24:09 -0700

    watchdog for sc_remoted

--- remote_watchdog.bash
+++ remote_watchdog.bash
@@ -1,12 +1,12 @@
 #check if program running
-if ! program="$(ps -aux | grep sc_remoted | grep allowother)"; then
+if ! program="$(ps -aux | grep sc_remoted | grep tka)"; then
 	logfile=/project/comcast-ping/bdrmap-bismark/sc_remoted.log.txt
 	#echo "not running"
 	date
 	echo "No program found. Restarting sc_remoted"
 	cd /home/agamerog/sockets; rm OW*
-	cd ~/scamper/bin
-	./sc_remoted -4 -P 31337 -U /project/comcast-ping/sockets-bismark/ -O allowother -O tka -O allowgroup >> $logfile 2>&1 &
+	cd /home/agamerog/scamper/scamper-backup-20160718/bin
+	./sc_remoted -4 -P 31337 -U /project/comcast-ping/sockets-bismark/ -O tka -O allowother -O allowgroup >> $logfile 2>&1 &
 #else
 	#date
 	#echo "running"
