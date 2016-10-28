9ecbf790e20eaf64170d7ccea53d73f946fa5119
Alexander Gamero-Garrido <gamero@alum.mit.edu>
2016-10-27 20:08:17 -0700

    new crawler for filenames

--- imc16plots/k_month_levelshift_filtering.sh
+++ imc16plots/k_month_levelshift_filtering.sh
@@ -4,7 +4,7 @@ ls *far*out > ~/far_list.txt
 for i in `cat ~/far_list.txt`;
 do
  ID="$(echo $i | awk -F"." '{print $4}')"
- for j in `ls *near*out | grep $ID`;
+ for j in `ls *$ID* | grep out | grep -v far`;
  do
   python /home/agamerog/pythoncode/python-bisrmak/imc16plots/filter_levelshift.py $i $j 
  done
